14d6e09a4f0993c0529a8cebaba4f6944e965463
Michael Gibson <michael.gibson@antithesis.com>
2024-02-09 15:10:54 -0500

    Cleanup and using a bit less memory

--- antithesis_sdk.h
+++ antithesis_sdk.h
@@ -1,9 +1,43 @@
 #pragma once
 
-#include <stdio.h>
+#include <cstdio>
 #include <string>
 #include <array>
 #include <source_location>
+
+namespace antithesis {
+    struct Assertion {
+        bool failed;
+        bool reached;
+        const char* message;
+        const char* function_name;
+        const char* file_name;
+        const int line;
+        std::string id;
+
+        Assertion(const char* message, const char* function_name, const char* file_name, const int line) : 
+            failed(false), reached(false), message(message), function_name(function_name), file_name(file_name), line(line), id(std::string(message) + " in " + function_name) {
+            this->add_to_catalog();
+        }
+
+        void add_to_catalog() const {
+            printf("There is an assertion with ID `%s` at %s:%d in `%s` with message: `%s`\n", id.c_str(), file_name, line, function_name, message);
+        }
+
+        [[clang::always_inline]] inline void check_assertion(bool cond) {
+            //(void)reached;  // Needed to cause registration to happen no matter what!
+            if (!reached) {
+                printf("The assertion with ID `%s` was reached\n", id.c_str());
+                reached = true;  // TODO: is the race OK?  If not, use a static initialization instead
+            }
+
+            if (!cond && !failed) {
+                printf("The assertion with ID `%s` failed: %s\n", id.c_str(), message);
+                failed = true;   // TODO: is the race OK?
+            }
+        }
+    };
+}
 
 namespace {
     template <std::size_t N>
@@ -23,18 +57,6 @@ namespace {
             return it;
         }
         const char* c_str() const { return contents.data(); }
-        constexpr size_t size() const { return N-1; }
-
-        template<std::size_t M>
-        constexpr fixed_string<N+M-1> concat( fixed_string<M> rhs ) const {
-            fixed_string< (N-1) + (M-1) + 1 > ret;
-            for(size_t i=0; i<N-1; i++)
-                ret.contents[i] = this->contents[i];
-            for(size_t i=0; i<M-1; i++)
-                ret.contents[N-1+i] = rhs.contents[i];
-            ret.contents[N-1 + M-1] = 0;
-            return ret;
-        }
     };
 
     static constexpr size_t string_length( const char * s ) {
@@ -43,48 +65,22 @@ namespace {
                 return l;
     }
 
-    template <class File, class FuncName>
-    struct src_location {
-        File file_name;
-        FuncName function_name;
-        int line;
-    };
-
-    template <fixed_string ID, fixed_string message, src_location loc >
+    template <fixed_string message, fixed_string file_name, fixed_string function_name, int line>
     struct CatalogEntry {
-        [[clang::always_inline]] static inline void check_assertion(bool cond) {
-            //(void)reached;  // Needed to cause registration to happen no matter what!
-            if (!reached) {
-                printf("The assertion with ID `%s` was reached\n", ID.c_str());
-                reached = true;  // TODO: is the race OK?  If not, use a static initialization instead
-            }
-
-            if (!cond && !failed) {
-                printf("The assertion with ID `%s` failed: %s\n", ID.c_str(), message.c_str());
-                failed = true;   // TODO: is the race OK?
-            }
-        }
-        [[clang::always_inline]] static inline void add_to_catalog() {
-            printf("There is an assertion with ID `%s` at %s:%d in `%s` with message: `%s`\n", ID.c_str(), loc.file_name.c_str(), loc.line, loc.function_name.c_str(), message.c_str());
+        [[clang::always_inline]] static inline antithesis::Assertion create() {
+            return antithesis::Assertion(message.c_str(), function_name.c_str(), file_name.c_str(), line);
         }
 
-        static inline bool reached = (CatalogEntry<ID,message,loc>::add_to_catalog(), false);
-        static inline bool failed = false;
+        static inline antithesis::Assertion assertion = create();
     };
 }
-
-#define STRINGIZE_DETAIL(x) #x
-#define STRINGIZE(x) STRINGIZE_DETAIL(x)
 
 #define FIXED_STRING_FROM_C_STR(s) (fixed_string<string_length(s)+1>::from_c_str(s))
 
 #define ALWAYS(cond, message) ( \
     CatalogEntry< \
-        fixed_string(message " in ").concat( FIXED_STRING_FROM_C_STR(std::source_location::current().function_name()) ), \
         fixed_string(message), \
-        src_location { \
-            FIXED_STRING_FROM_C_STR(std::source_location::current().file_name()), \
-            FIXED_STRING_FROM_C_STR(std::source_location::current().function_name()), \
-            std::source_location::current().line() \
-        } \
-    >::check_assertion(cond) )
+        FIXED_STRING_FROM_C_STR(std::source_location::current().file_name()), \
+        FIXED_STRING_FROM_C_STR(std::source_location::current().function_name()), \
+        std::source_location::current().line() \
+    >::assertion.check_assertion(cond) )
