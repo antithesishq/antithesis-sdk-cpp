0dd21d57549df9b16def3922bd1a1d6f392e7dd5
Bronek Kozicki <brok@incorrekt.com>
2024-10-21 21:24:42 +0100

    Add ALWAYS and UNREACHABLE polyfills

--- antithesis_sdk.h
+++ antithesis_sdk.h
@@ -4,6 +4,7 @@
 //
 // Documentation for the SDK is found at https://antithesis.com/docs/using_antithesis/sdk/cpp/overview.html.
 
+#include <cassert>
 #include <cstdint>
 #include <random>
 #include <string>
@@ -69,20 +70,23 @@ namespace antithesis {
 #endif
 #endif
 
-#define ALWAYS(cond, message, ...)
-#define ALWAYS_OR_UNREACHABLE(cond, message, ...)
+#define ALWAYS(cond, message, ...) assert((message) && (cond))
+#define ALWAYS_OR_UNREACHABLE(cond, message, ...) assert((message) && (cond))
 #define SOMETIMES(cond, message, ...)
 #define REACHABLE(message, ...)
-#define UNREACHABLE(message, ...)
-#define ALWAYS_GREATER_THAN(val, threshold, message, ...)
-#define ALWAYS_GREATER_THAN_OR_EQUAL_TO(val, threshold, message, ...)
+#define UNREACHABLE(message, ...) assert((message) && false)
+#define ALWAYS_GREATER_THAN(val, threshold, message, ...) assert((message) && (val > threshold) && (cond))
+#define ALWAYS_GREATER_THAN_OR_EQUAL_TO(val, threshold, message, ...) assert((message) && (val >= threshold) && (cond))
 #define SOMETIMES_GREATER_THAN(val, threshold, message, ...)
 #define SOMETIMES_GREATER_THAN_OR_EQUAL_TO(val, threshold, message, ...)
-#define ALWAYS_LESS_THAN(val, threshold, message, ...)
-#define ALWAYS_LESS_THAN_OR_EQUAL_TO(val, threshold, message, ...)
+#define ALWAYS_LESS_THAN(val, threshold, message, ...) assert((message) && (val < threshold) && (cond))
+#define ALWAYS_LESS_THAN_OR_EQUAL_TO(val, threshold, message, ...) assert((message) && (val <= threshold) && (cond))
 #define SOMETIMES_LESS_THAN(val, threshold, message, ...)
 #define SOMETIMES_LESS_THAN_OR_EQUAL_TO(val, threshold, message, ...)
-#define ALWAYS_SOME(pairs, message, ...)
+#define ALWAYS_SOME(pairs, message, ...) assert((message) && [&](){ \
+    for (std::pair<std::string, bool> pair : pairs) \
+        if (pair.second) return true; \
+    return false; }())
 #define SOMETIMES_ALL(pairs, message, ...)
 
 namespace antithesis {
